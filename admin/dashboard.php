9f922be5b7aa59f61467ebe161303efc482d1e2c
ansonlee941031 <ansonlee1031@gmail.com>
2025-11-27 21:52:14 +0800

    1

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -1,13 +1,21 @@
 <?php
 session_start();
+
 if (!isset($_SESSION['user_id']) || $_SESSION['role'] !== 'admin') {
     header('Location: ../index.php');
     exit;
-
-require 'includes/header.php';
-
 }
+
+require '../includes/header.php'; 
+
 ?>
-<h1>管理員首頁</h1>
-<p>哈囉，<?php echo htmlspecialchars($_SESSION['name']); ?>！</p>
-<a href="../logout.php">登出</a> 
+
+<div class="container mt-4">
+    <h1>管理員首頁</h1>
+    <p>哈囉，<?php echo htmlspecialchars($_SESSION['name']); ?>！</p>
+    <a href="../logout.php">登出</a> 
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,3 @@
-
 <?php
 $title=$title??"活動系統";
 function nav_active($file) {
@@ -16,38 +15,37 @@ function nav_active($file) {
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">    
     <link rel="stylesheet" href="https://cdn.datatables.net/2.0.8/css/dataTables.bootstrap5.css">
     
-    <link rel="stylesheet" href="includes/custom.css" />
+    <link rel="stylesheet" href="../includes/custom.css" /> 
 </head>
 
 <body class="bg-light">
   <nav class="navbar navbar-expand-lg custom-bg">
     <div class="container">
-      <a class="navbar-brand" href="activity.php">包裹/公物系統</a>
+      <a class="navbar-brand" href="../activity.php">包裹/公物系統</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item">
-            <a class="nav-link<?=nav_active('pack.php')?>" href="package.php">包裹管理</a>
+            <a class="nav-link<?=nav_active('pack.php')?>" href="../package.php">包裹管理</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link<?=nav_active('property.php')?>" href="public.php">公物管理</a>
+            <a class="nav-link<?=nav_active('property.php')?>" href="../public.php">公物管理</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link<?=nav_active('job.php')?>" href="job.php">包裹公物狀態</a>
+            <a class="nav-link<?=nav_active('job.php')?>" href="../job.php">包裹公物狀態</a>
           </li>
         </ul>
 
         <ul class="navbar-nav mb-2 mb-lg-0">
         <ul class="navbar-nav mb-2 mb-lg-0">
-        <?php if(isset($_SESSION['account'])): ?>
-          <li class="nav-item">
-            <a class="nav-link" href="logout.php">登出</a>
+<?php if(isset($_SESSION['username'])): ?>          <li class="nav-item">
+            <a class="nav-link" href="../logout.php">登出</a>
           </li>
         <?php else: ?>
           <li class="nav-item">
-            <a class="nav-link" href="login.php">登入</a>
+            <a class="nav-link" href="../login.php">登入</a>
           </li>
         <?php endif; ?>
       </ul>
@@ -55,4 +53,3 @@ function nav_active($file) {
       </div>
     </div>
   </nav>
- 
