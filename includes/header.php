8b4c314b69b3aa4b8b8664d07ef507003f012936
ansonlee941031 <ansonlee1031@gmail.com>
2025-11-27 22:52:34 +0800

    20251127 Anson

--- includes/header.php
+++ includes/header.php
@@ -21,7 +21,7 @@ function nav_active($file) {
 <body class="bg-light">
   <nav class="navbar navbar-expand-lg custom-bg">
     <div class="container">
-      <a class="navbar-brand" href="../activity.php">包裹/公物系統</a>
+      <a class="navbar-brand">包裹/公物系統</a>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
