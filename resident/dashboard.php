76a6c78cbb809da56191b31db0563b9e33391a50
ansonlee941031 <ansonlee1031@gmail.com>
2025-12-02 14:43:35 +0800

    Add files via upload

--- resident/dashboard.php
+++ resident/dashboard.php
@@ -6,6 +6,7 @@ if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'resident') {
     exit;
 }
 
+$path = "../"; 
 require '../includes/header.php'; 
 
 ?>
@@ -19,23 +20,22 @@ require '../includes/header.php';
     </div>
 
     <div class="row">
-
         <div class="col-md-6 mb-4">
             <div class="card shadow-sm h-100">
                 <div class="card-header custom-bg text-white fw-bold">
                     包裹公物狀態
                 </div>
                 <div class="card-body">
-                    <h5 class="card-title">包裹與公物租借狀態</h5>
+                    <h5 class="card-title">查詢狀態</h5>
                     <p class="card-text">
-                        <strong>查看包裹清單 / 查看公物租借狀況</strong>
+                        <strong>查看您的包裹清單 / 查看公物目前租借狀況</strong>
                     </p>
-                    <a href="../state.php" class="btn btn-primary w-100">前往包裹公物狀態頁</a>
+                    <a href="../state.php" class="btn btn-primary w-100">前往查詢頁面</a>
                 </div>
             </div>
         </div>
     </div>
-    </div>
+</div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
