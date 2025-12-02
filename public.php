b902939f2fbc250c16d18ebf5f5f4459390f7a09
Anson <ansonlee1031@gmail.com>
2025-12-02 13:34:56 +0800

    20251202 Anson

--- public.php
+++ public.php
@@ -6,7 +6,7 @@ require 'includes/header.php';
 if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
     echo "<script>
             alert('僅管理員可進入公物管理頁面。');
-            window.location.href = 'index.php';
+            window.location.href = 'resident/dashboard.php';
           </script>";
     exit;
 }
